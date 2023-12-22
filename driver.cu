3acaf177bf8620d8abe7cf4429c5d50a96e2eae0
eliphatfs <flandre@scarletx.cn>
2023-12-21 16:10:16 -0800

    Add sign phase to driver.

--- driver.cu
+++ driver.cu
@@ -20,26 +20,31 @@ int main(int argc, char ** argv)
     for (int i = 0; i < F; i++)
         for (int j = 0; j < 3; j++)
             fi >> tris[3 * i + j].x >> tris[3 * i + j].y >> tris[3 * i + j].z;
-    cudaDeviceSynchronize();
+    fi.close();
     const auto inputPhase = clock.now() - start;
     start = clock.now();
 
     // for (int i = 0; i < buffer.size() / 3 / sizeof(float); i++)
     //     printf("%.2f %.2f %.2f\n", tris[i].x, tris[i].y, tris[i].z);
 
-    RasterizeResult rast = rasterize_tris(tris, F, 128, 4.0f / 128);
-    cudaDeviceSynchronize();
+    constexpr const int N = 128;
+    RasterizeResult rast = rasterize_tris(tris, F, N, 4.0f / N);
     const auto rasterizePhase = clock.now() - start;
     start = clock.now();
-
+    fill_signs(tris, N, rast);
+    const auto signPhase = clock.now() - start;
+    start = clock.now();
+    static char _output_buf[2 * 1024 * 1024];
     std::ofstream fo("output.txt", std::fstream::trunc);
-    for (int i = 0; i < 128; i++)
-        for (int j = 0; j < 128; j++)
+    fo.rdbuf()->pubsetbuf(_output_buf, 2 * 1024 * 1024);
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
         {
-            for (int k = 0; k < 128; k++)
-                fo << rast.gridDist[i * 128 * 128 + j * 128 + k] << '\t';
+            for (int k = 0; k < N; k++)
+                fo << rast.gridDist[i * N * N + j * N + k] << '\t';
             fo << '\n';
         }
+    fo.close();
     cudaFree(tris);
     rast.free();
     const auto outputPhase = clock.now() - start;
@@ -47,6 +52,7 @@ int main(int argc, char ** argv)
 
     std::clog << "[Timing] Input phase: " << (int)(inputPhase.count() / 1e6) << " ms" << std::endl;
     std::clog << "         Rasterize phase: " << (int)(rasterizePhase.count() / 1e6) << " ms" << std::endl;
+    std::clog << "         Sign phase: " << (int)(signPhase.count() / 1e6) << " ms" << std::endl;
     std::clog << "         Output phase: " << (int)(outputPhase.count() / 1e6) << " ms" << std::endl;
     return 0;
 }
