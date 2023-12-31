59e6805f7bcdc7adb9431ffe97a36127ef3f4b22
eliphatfs <flandre@scarletx.cn>
2023-12-30 19:06:32 -0800

    Fix.

--- torchcumesh2sdf.cu
+++ torchcumesh2sdf.cu
@@ -6,7 +6,7 @@ at::Tensor get_sdf(const at::Tensor tris, const int R, const float band, const i
     assert(tris.sizes()[1] == 3);
     assert(tris.sizes()[2] == 3);
     auto rast = rasterize_tris((float3*)tris.data_ptr<float>(), tris.sizes()[0], R, band, B, true);
-    fill_signs((float3*)tris.data_ptr<float>(), R, rast);
+    fill_signs((float3*)tris.data_ptr<float>(), R, rast, true);
     int device;
     CHECK_CUDA(cudaGetDevice(&device));
     auto options = torch::TensorOptions().dtype(torch::kFloat32).device(torch::kCUDA, device);
