34fa33d246d63d8c8cf1152e33811f317bbc00c7
eliphatfs <flandre@scarletx.cn>
2023-12-30 17:22:08 -0800

    Slightly better support for multi-gpu systems.

--- torchcumesh2sdf.cu
+++ torchcumesh2sdf.cu
@@ -7,7 +7,9 @@ at::Tensor get_sdf(const at::Tensor tris, const int R, const float band)
     assert(tris.sizes()[2] == 3);
     auto rast = rasterize_tris((float3*)tris.data_ptr<float>(), tris.sizes()[0], R, band);
     fill_signs((float3*)tris.data_ptr<float>(), R, rast);
-    auto options = torch::TensorOptions().dtype(torch::kFloat32).device(torch::kCUDA, 0);
+    int device;
+    CHECK_CUDA(cudaGetDevice(&device));
+    auto options = torch::TensorOptions().dtype(torch::kFloat32).device(torch::kCUDA, device);
     at::Tensor result = torch::from_blob(rast.gridDist, { R, R, R }, options).clone();
     rast.free();
     return result;
